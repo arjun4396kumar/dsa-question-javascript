87da680e1f676cfbbc4a3a9556a9fc1e65ee287d
arjun4396kumar <43697473+arjun4396kumar@users.noreply.github.com>
2024-11-06 10:59:39 +0530

    Update print_1_to_n.js

--- Recursion/print_1_to_n.js
+++ Recursion/print_1_to_n.js
@@ -1,1 +1,18 @@
 
+//Method 1
+function print_one_to_n(n){
+    if(n == 0) return 1
+    print_one_to_n(n-1)
+    console.log(n)
+}
+print_one_to_n(10)
+
+// Method 2
+function print_one_to_n_v2(n){
+    if(n>0){
+        print_one_to_n_v2(n-1)
+        console.log(n)
+    }
+}
+print_one_to_n_v2(10)
+
